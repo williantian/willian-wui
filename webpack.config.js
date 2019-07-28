1832f547423c6faf4c979a435e68c209e1746f09
Your Name <you@example.com>
2019-07-28 16:30:10 +0800

    修改webpack.config.js中的书写错误 安装typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+const path = require('path');
 module.exports = {
+   mode: 'production', //值是development或production 正在开发或者进行发布
    entry: {
        index: './lib/index.tsx' //为安全起见从./开始写 后面接路径
    },
-   modules: {
+   output: {
+       path: path.resolve(__dirname, 'dist/lib')
+       // 所有输出文件的目标路径
+       // 必须是绝对路径（使用 Node.js 的 path 模块）
+   },
+   module: {
        rules:[
            {
-               text: /\.tsx?$/,
+               test: /\.tsx?$/,
                loader: 'awesome-typescript-loader'//loader需要安装
            }
        ]
