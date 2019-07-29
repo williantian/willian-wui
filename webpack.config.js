2bc9a6e8cabe806fee12f9ede7a3ad327bf83ffd
Your Name <you@example.com>
2019-07-29 14:33:58 +0800

    添加resolve externals

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ module.exports = {
     entry: {
         index: './lib/index.tsx' //为安全起见从./开始写 后面接路径
     },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        //后缀是这些的文件 都会去找
+    },
     output: {
         path: path.resolve(__dirname, 'dist/lib')
         // 所有输出文件的目标路径
@@ -22,5 +26,20 @@ module.exports = {
                 loader: 'awesome-typescript-loader'//loader需要安装
             }
         ]
+    },
+    externals: { //externals 内的库是属于外部的库
+        react: {
+            commonjs: 'react', //module.exports={} 引用var React=require('react')括号内可配置
+            commonjs2: 'react',  //nodejs标准  modules.export
+            amd: 'react',       //浏览器标准
+            root: 'React', // 两者都不是 <script src="xx/react.js"></script>
+            //window.react = {} window.xxx可以配置
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM',
+        },
     }
 }
