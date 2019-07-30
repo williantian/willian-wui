9f584191a12abde0f88946ecf8b402edb15f8f7d
Your Name <you@example.com>
2019-07-30 09:47:07 +0800

    添加jest测试

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    verbose: true,//显示所有的错误
+    clearMocks: false,
+    collectCoverage: false,  //是否收集测试覆盖率
+    reporters: ["default"],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleDirectories: ['node_modules'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.test.json',
+        },
+    },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+        "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
+    },
+    testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
+    //所有测试文件在哪里 和规定格式
+    transform: {
+        "^.+unit\\.(js|jsx)$": "babel-jest",
+        //转译 如果是js/jsx就用babel-jest
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+        //如果是ts/tsx就用ts-jest
+    },
+    setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+}
---
+++ lib/__tests__/button.unit.tsx
@@ -0,0 +1,12 @@
+import * as renderer from 'react-test-renderer'
+import * as React from 'react'
+import Button from '../button'
+
+describe('button', () => {
+    it('是个div', () => {
+        const json = renderer.create(<Button />).toJSON()
+        //const json = renderer.create(React.createElement('div',null,'hi')).toJSON()
+        //不引入Button就能正常运行         ????????  bug ????未解决
+        expect(json).toMatchSnapshot()
+    })
+})
