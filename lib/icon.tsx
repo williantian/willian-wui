cdff7dcdecf63286caa7381f58bb897075a7e158
Your Name <you@example.com>
2019-07-30 16:10:36 +0800

    添加onClick事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,12 +3,14 @@ import './importIcons';
 import './icon.scss';
 
 interface IconProps {
-    name: string 
+    name: string ,
+    onClick: ()=>void
 }
+
 // Icon:React.FunctionComponent<IconProps> 添加name属性到props
 const Icon:React.FunctionComponent<IconProps> = (props)=>{
     return (
-        <svg className = 'icon'>
+        <svg className = 'icon' onClick = {props.onClick}>
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-
+const fn = ()=>{
+    console.log('fn')
+}
 ReactDOM.render(
     <div>
-        <Icon name='wechat' />
-        <Icon name='alipay' />
-        <Icon name='qq' />
+        <Icon name='wechat' onClick = {fn} />
     </div>,
     document.getElementById('root')
 );
