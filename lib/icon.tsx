02815590b5f49733fd50b7094683a47c21464238
Your Name <you@example.com>
2019-07-30 17:12:06 +0800

    完善onClick

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,15 +3,16 @@ import './importIcons';
 import './icon.scss';
 
 interface IconProps {
-    name: string ,
-    onClick: ()=>void
+    name: string ;
+    onClick: React.MouseEventHandler<SVGElement>
+     // 如果index.tsx函数声明出为React.MouseEventHandler<SVGElement> 则此不能写成React.MouseEventHandler<SVGElement> 否则会提示有冲突
+     //同理如果index.tsx函数声明出为React.MouseEventHandler 则只能写作React.MouseEventHandler<SVGElement> 不能相同
 }
-
 // Icon:React.FunctionComponent<IconProps> 添加name属性到props
-const Icon:React.FunctionComponent<IconProps> = (props)=>{
+const Icon: React.FunctionComponent<IconProps> = (props)=>{
     return (
-        <svg className = 'icon' onClick = {props.onClick}>
-            <use xlinkHref={`#${props.name}`}></use>
+        <svg className = 'wui-icon' onClick = {props.onClick}>
+            <use xlinkHref={`#${props.name}`} />
         </svg>
     )
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-const fn = ()=>{
-    console.log('fn')
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e.target)
+    console.log('hi')
 }
 ReactDOM.render(
     <div>
